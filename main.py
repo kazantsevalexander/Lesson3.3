2526500e9b3192c685122bf7333ec675ebfe53f2
kazantsevalexander <kazantsevalexander@gmail.com>
2024-09-07 01:05:40 +0300

    add target img

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Меткий стрелок")
-icon = pygame.image.load("")
+icon = pygame.image.load("img/2689911.png")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("")
 
 running = True
 while running:
