4dcf1b587c8993d135c9471fae4108e6786b184d
kazantsevalexander <kazantsevalexander@gmail.com>
2024-09-07 01:14:47 +0300

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -10,7 +11,14 @@ pygame.display.set_caption("Меткий стрелок")
 icon = pygame.image.load("img/2689911.png")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("")
+target_img = pygame.image.load("img/target.png")
+target_width = 80
+target_height = 80
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color_bg = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
