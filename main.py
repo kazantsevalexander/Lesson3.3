b5a47ccfcc3311085ea9200995bf1bb8a4b596bf
kazantsevalexander <kazantsevalexander@gmail.com>
2024-09-07 13:50:51 +0300

    add darts and count

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ pygame.display.set_caption("Меткий стрелок")
 icon = pygame.image.load("img/2689911.png")
 pygame.display.set_icon(icon)
 
+cursor_img = pygame.image.load("img/darts.png")
+cursor_rect = cursor_img.get_rect()
+pygame.mouse.set_visible(False)
+
 target_img = pygame.image.load("img/target.png")
 target_width = random.randint(30, 150)
 target_height = target_width
@@ -23,20 +27,29 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color_bg = (random.randint(50, 100), random.randint(50, 200), random.randint(50, 200))
 
+font = pygame.font.Font(None, 26)  # Шрифт для отображения счета
+score = 0
+strel = 10
+
 running = True
 
-target_speed = 5  # Скорость перемещения мишени
+target_speed = 10  # Скорость перемещения мишени
 target_new_x = target_x
 target_new_y = target_y
 
 while running:
     screen.fill(color_bg)
+    # Получаем позицию мыши
+
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or strel == 0:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
+            strel -= 1
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                score += 1
                 # Получаем новые случайные координаты и размеры
                 target_width = random.randint(30, 150)
                 target_height = target_width
@@ -47,10 +60,17 @@ while running:
 
     # Плавное перемещение мишени
     if target_x != target_new_x or target_y != target_new_y:
-        target_x += (target_new_x - target_x) / target_speed
+        target_x += (target_new_x - target_x) / target_speed ** 2
         target_y += (target_new_y - target_y) / target_speed
 
     screen.blit(scaled_img, (target_x, target_y))
+    cursor_rect.topleft = pygame.mouse.get_pos()
+
+    # Отображение счета
+    score_text = font.render(f"Счет: {score} Осталось стрел: {strel}", True, (255, 255, 255))
+    screen.blit(score_text, (20, 10))
+
+    screen.blit(cursor_img, cursor_rect)
     pygame.display.update()
 
 pygame.quit()
